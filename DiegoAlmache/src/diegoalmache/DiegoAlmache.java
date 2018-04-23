c7d790fc886ed6f7d633cd60b6a04a83b88e7215
Estudiante <Estudiante@LAB06-PC03>
2018-04-23 11:02:49 -0500

    Mi Clase Marco

--- DiegoAlmache/src/diegoalmache/DiegoAlmache.java
+++ DiegoAlmache/src/diegoalmache/DiegoAlmache.java
@@ -15,7 +15,9 @@ public class DiegoAlmache {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("EL Mejor Programador del Mundo");
     }
+    
+    
     
 }
